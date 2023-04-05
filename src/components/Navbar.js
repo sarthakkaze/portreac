bc77ec5992ca53d7f7ca8f296830ab0ef21d6b93
Kaze <kaze@SarthaksMac.local>
2023-04-05 18:02:13 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../assests/style/navbar.css"
+
+function Navbar(){
+
+
+    return(
+        <div className='wrapper'>
+            <div className='bar' >
+                <p className='p-0 m-0'>NAVBAR</p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Navbar;
